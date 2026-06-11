3f686b62c152d96ed15b28d51e652810de606a6c
inkoreakim <rlawngks007@gmail.com>
2026-06-11 17:06:54 +0900

    swagger 문서 명 수정

--- src/main/java/com/eis_project/EisProjectApplication.java
+++ src/main/java/com/eis_project/EisProjectApplication.java
@@ -1,8 +1,11 @@
 package com.eis_project;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(title = "EIS API 문서"))
 @SpringBootApplication
 public class EisProjectApplication {
 
